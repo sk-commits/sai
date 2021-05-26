38b079155cf17046aefa148b6465dcdf486c9311
sai <sai@192.168.0.5>
2021-05-26 13:44:11 +0530

    Javascript Reg Form

---
+++ weapp/src/webapp/servlets/MyFormServlet.java
@@ -0,0 +1,52 @@
+package webapp.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyFormServlet
+ */
+@WebServlet("/MyFormServlet")
+public class MyFormServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyFormServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String FName=request.getParameter("FName");
+		String LName=request.getParameter("LName");
+		
+		
+		PrintWriter out = response.getWriter();
+		out.println("Username"+FName);
+		out.println("Username"+LName);
+			
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
